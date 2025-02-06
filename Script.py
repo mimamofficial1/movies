8ef88f0c3ee10ddf784837832424f5ea686cd4e2
mimamofficial1 <muzaffarimamneema@gmail.com>
2025-02-07 00:02:51 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,9 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/Jisshu_Originals">𝙅𝙞𝙨𝙨𝙝𝙪 𝙊𝙧𝙞𝙜𝙞𝙣𝙖𝙡𝙨</a>」</b>"""
+    CAPTION = """<b>❤‍🔥𓊈𝖒𝖗𝖓_𝖗𝖎𝖕𝖕𝖊𝖗𓊉 : <b>{file_caption}</b>
+
+⚙️ sɪᴢᴇ : {file_size
     
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
 
