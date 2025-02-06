eafbec0ba0988104948df0ad81afe5c4c1731b9b
mimamofficial1 <muzaffarimamneema@gmail.com>
2025-02-07 00:34:22 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-   CAPTION = """<b>{file_caption}</b>"""
+   FILE_CAPTION = """<b>{file_caption}</b>"""
     
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
 
